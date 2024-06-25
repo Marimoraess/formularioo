b364dd5c7ee4d3f728d9a08d753fb40f89ff5897
Marimoraess <moraesmariana2007@gmail.com>
2024-06-25 10:08:17 -0300

    atv4

---
+++ atv4/form.php
@@ -0,0 +1,14 @@
+<?php
+
+$ano = $_GET['ano'];
+$calcul1 = $ano%4;
+$calcul2 = $ano%100;
+$calcul3 = $ano%400;
+
+if($calcul1 ==0 && $calcul2 =! 0){
+    echo "O ano $ano é bissexto!";
+}
+else if($calcul3 == 0){
+    echo "O ano $ano é bissexto!";
+}
+?>
