b0d14b3f21c1d05bfa90246a135938d37e63b21e
Marimoraess <moraesmariana2007@gmail.com>
2024-06-25 10:25:11 -0300

    atv6

--- atv6/form.php
+++ atv6/form.php
@@ -4,25 +4,25 @@ $n1 = $_GET['num1'];
 $n2 = $_GET['num2'];
 $frequencia = $_GET['freq'];
 $media = ($n1+$n2)/2;
-echo "Nome: $n<br>";
+echo "Nome: $nm<br>";
 echo "<br>";
 echo "Nota 1: $n1<br>";
 echo "<br>";
 echo "Nota 2: $n2<br>";
 echo "<br>";
-echo "Frequência: $f<br>";
+echo "Frequência: $frequencia<br>";
 echo "<br>";
-echo "Média: $med<br>";
+echo "Média: $media<br>";
 echo "<br>";
 
 if($media < 5){
     echo "Situação: Reprovado(a) por nota";
     $calc = $n1+$n2;
     }
-if($f < 75){
+if($frequencia < 75){
     echo "Situação: Reprovado(a) por frequência";
     }
-if($med >= 5 && $f >= 75){
+if($media >= 5 && $f >= 75){
     echo "Situação: Aprovado<br>";
     }
 
