d4fe503de98afa6a372d74488c9cfceff16c9246
Marimoraess <moraesmariana2007@gmail.com>
2024-06-24 21:39:33 -0300

    atv2

--- exemplo2/form.php
+++ exemplo2/form.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3107 Beecrowd</title>
-</head>
-<body>
+
     <?php
         $veloc = $_POST['veloc'];
         $dist= $_POST['dist'];
@@ -18,5 +10,3 @@
         echo "Total horas: $horas\n";
         echo "Total combustivel: $comb\n";
     ?>
-</body>
-</html>
