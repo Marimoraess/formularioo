85a103b354e10fb569316cc5df4aa75d8c02ed10
Marimoraess <moraesmariana2007@gmail.com>
2024-06-25 10:39:03 -0300

    atv8

---
+++ atv9/form.php
@@ -0,0 +1,15 @@
+<?php
+$nome =$_GET['nome'];
+$horario = $_GET['horario'];
+
+list($hora, $minuto) = explode(':', $horario);
+
+if ($hora < 12) {
+    echo "Bom dia $nome!<br>";
+} elseif ($hora < 18) {
+    echo "Boa tarde $nome!<br>";
+} else {
+    echo "Boa noite $nome!<br>";
+}
+echo "São $hora e $minuto minutos.<br>"
+?>
